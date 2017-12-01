e1967f4d5af70bacac6e6fe264d7d95ac20cd23b
pvdasko <pvdasko@hotmail.com>
2017-12-01 17:43:06 -0600

    update 011217 2

---
+++ Pos_WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pos_WebApplication.Startup))]
+namespace Pos_WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
